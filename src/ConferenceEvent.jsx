bf2ee2e4659247c41323457555b4f2edcaa9aff1
Onel-R <leonreuben@yahoo.com>
2025-05-16 20:35:08 -0400

    Meals Table

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -52,13 +52,67 @@ const ConferenceEvent = () => {
 
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+            if(item.quantity > 0) {
+                items.push({...item, type: "venue"});
+            }
+        });
+        avItems.forEach((item) => {
+            if(item.quantity > 0 &&
+                !items.some((i) => i.name === item.name && i.type === "av")) {
+                items.push({...item, type: "av"});
+            }
+        });
+        mealsItems.forEach((item) => {
+            if(item.selected) {
+                const itemForDisplay = {...item, type: "meals"};
+                if (item.numberOfPeople) {
+                    itemForDisplay.numberOfPeople = numberOfPeople;
+                  }
+                  items.push(itemForDisplay);
+            }
+        });
+        return items;
     };
 
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
-
+        console.log(items);
+        return <>
+            <div className="display_box1">
+                {items.length === 0 && <p>No items selected</p>}
+                <table className="table_item_data">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Unit Cost</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>${item.cost}</td>
+                                <td>
+                                    {item.type === "meals" || item.numberOfPeople
+                                    ? ` For ${numberOfPeople} people`
+                                    : item.quantity}
+                                </td>
+                                <td>{item.type === "meals" || item.numberOfPeople
+                                    ? `${item.cost * numberOfPeople}`
+                                    : `${item.cost * item.quantity}`}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     };
+
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
@@ -81,6 +135,12 @@ const ConferenceEvent = () => {
     const venueTotalCost = calculateTotalCost("venue");
     const avTotalCost = calculateTotalCost("av");
     const mealsTotalCost = calculateTotalCost("meals");
+    
+    const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost,
+    };
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -194,16 +254,11 @@ const ConferenceEvent = () => {
         </div>
         <div className="total_cost">Total Cost: ${avTotalCost}</div>
         </div>
-
                             {/* Meal Section */}
-
                             <div id="meals" className="venue_container container_main">
-
                                 <div className="text">
-
                                     <h1>Meals Selection</h1>
                                 </div>
-
                                 <div className="input-container venue_selection">
                                 <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
                                     <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
@@ -224,11 +279,8 @@ const ConferenceEvent = () => {
                                             <div className="meal_cost">${item.cost}</div>
                                         </div>
                                     ))}
-
                                 </div>
                                 <div className="total_cost">Total Cost: ${mealsTotalCost}</div>
-
-
                             </div>
                         </div>
                     ) : (
